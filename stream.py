aa02e372daafdf98ed09216372ba12c19fe5f42f
bonkizu <isukaomu@gmail.com>
2024-12-10 11:13:54 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -128,8 +128,7 @@ def perform_similarity_search(query, model, index, df, k, collection):
     results = []
     for i, idx in enumerate(indices[0]):
         # Get the MongoDB _id from df and convert to ObjectId
-        print(idx)
-        document_id = load_index_dict()[idx]
+        document_id = load_index_dict()[0]
         
         # Query MongoDB to get the document by _id
         document = collection.find_one({"_id": ObjectId(document_id)})
